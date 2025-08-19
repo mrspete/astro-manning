2767d3415a621d8198f717c1c00b1450b2bf59a9
Manning Peterson <manning.peterson@gmail.com>
2025-08-18 19:21:02 -0500

    added github action deploy files

---
+++ digital-debris/astro.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+    site: 'https://mrspete.github.io/astro-manning/',
+    base: '/astro-manning/',
+});
